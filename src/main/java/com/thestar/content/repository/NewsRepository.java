b5dd9a3d773041d27b801467022fbbfd411a170d
ken86tw <72748203+ken86tw@users.noreply.github.com>
2026-07-06 15:27:16 +0800

    Revert "新增後台員工登入前台最新消息"

--- src/main/java/com/thestar/content/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thestar.content.repository;
-
-import com.thestar.content.entity.NewsVO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<NewsVO, Integer> {
-}
