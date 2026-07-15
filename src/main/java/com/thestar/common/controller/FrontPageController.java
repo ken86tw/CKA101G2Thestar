95dda71772f2798a80e4083c65c049891b447591
dillen <dillen99999999@gmail.com>
2026-07-15 03:52:24 -0700

    修正首頁文章顯示

--- src/main/java/com/thestar/common/controller/FrontPageController.java
+++ src/main/java/com/thestar/common/controller/FrontPageController.java
@@ -1,13 +1,22 @@
 package com.thestar.common.controller;
 
+import com.thestar.content.service.ContentAdminService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontPageController {
 
+    private final ContentAdminService contentAdminService;
+
+    public FrontPageController(ContentAdminService contentAdminService) {
+        this.contentAdminService = contentAdminService;
+    }
+
     @GetMapping({"/", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("latestNews", contentAdminService.findLatestNews());
         return "index";
     }
 
